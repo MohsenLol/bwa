326d5675acd6293f3e274aa59d6ccec75bf6dbba
PSalehi <pouyasalehieleceng@gmail.com>
2026-02-05 19:33:50 +0300

    added bns_pos2rid\_gpu and bns_intv2rid_gpu into cu.file

--- cuda/bntseq_CUDA.cu
+++ cuda/bntseq_CUDA.cu
@@ -4,27 +4,6 @@
 #define _set_pac(pac, l, c) ((pac)[(l)>>2] |= (c)<<((~(l)&3)<<1))
 #define _get_pac(pac, l) ((pac)[(l)>>2]>>((~(l)&3)<<1)&3)
 
-__forceinline__ __device__ int bns_pos2rid_gpu(const bntseq_t *bns, int64_t pos_f)
-{
-	int left, mid, right;
-	if (pos_f >= bns->l_pac) return -1;
-	left = 0; mid = 0; right = bns->n_seqs;
-	while (left < right) { // binary search
-		mid = (left + right) >> 1;
-		if (pos_f >= bns->anns[mid].offset) {
-			if (mid == bns->n_seqs - 1) break;
-			if (pos_f < bns->anns[mid+1].offset) break; // bracketed
-			left = mid + 1;
-		} else right = mid;
-	}
-	return mid;
-}
-
-__forceinline__ __device__ static  int64_t bns_depos_gpu(const bntseq_t *bns, int64_t pos, int *is_rev)
-{
-	return (*is_rev = (pos >= bns->l_pac))? (bns->l_pac<<1) - 1 - pos : pos;
-}
-
 // rb : interval  begin in refrence 
 // re : interval  end in refrence 
 // ... [rb......re] ....
--- cuda/bntseq_CUDA.cuh
+++ cuda/bntseq_CUDA.cuh
@@ -1,7 +1,32 @@
 #include <stdint.h>
 #include "../bntseq.h"
 
-extern __device__ int bns_pos2rid_gpu(const bntseq_t *bns, int64_t pos_f);
-extern __device__ int bns_intv2rid_gpu(const bntseq_t *bns, int64_t rb, int64_t re);
+
+
+__forceinline__ __device__ bns_pos2rid_gpu(const bntseq_t *bns, int64_t pos_f);
+__forceinline__ __device__ static  bns_intv2rid_gpu(const bntseq_t *bns, int64_t rb, int64_t re);
 extern __device__ uint8_t *bns_fetch_seq_gpu(const bntseq_t *bns, const uint8_t *pac, int64_t *beg, int64_t mid, int64_t *end, int *rid, void* d_buffer_ptr);
 extern __device__ uint8_t *bns_get_seq_gpu(int64_t l_pac, const uint8_t *pac, int64_t beg, int64_t end, int64_t *len, void* d_buffer_ptr);
+
+
+__forceinline__ __device__ int bns_pos2rid_gpu(const bntseq_t *bns, int64_t pos_f)
+{
+	int left, mid, right;
+	if (pos_f >= bns->l_pac) return -1;
+	left = 0; mid = 0; right = bns->n_seqs;
+	while (left < right) { // binary search
+		mid = (left + right) >> 1;
+		if (pos_f >= bns->anns[mid].offset) {
+			if (mid == bns->n_seqs - 1) break;
+			if (pos_f < bns->anns[mid+1].offset) break; // bracketed
+			left = mid + 1;
+		} else right = mid;
+	}
+	return mid;
+}
+
+__forceinline__ __device__ static  int64_t bns_depos_gpu(const bntseq_t *bns, int64_t pos, int *is_rev)
+{
+	return (*is_rev = (pos >= bns->l_pac))? (bns->l_pac<<1) - 1 - pos : pos;
+}
+
