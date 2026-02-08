b2c36851b18dea77ae0c1c1b9f21817345c7d672
PSalehi <pouyasalehieleceng@gmail.com>
2026-02-08 23:22:38 +0300

    new kernel : 	CHAINFILTERING_flt_chained_seeds_kernel<<<flt_numBlocks, flt_blockSize, 0, process_stream>>>( 		d_opt, d_bns, d_pac, d_seqs, d_chains, n, d_buffer_pools 	);

--- cuda/bwamem_GPU.cu
+++ cuda/bwamem_GPU.cu
@@ -3149,7 +3149,7 @@ struct FltSharedLayout {
 };
 
 
-__global__ void CHAINFILTERING_flt_chained_seeds_kernel_new(
+__global__ void CHAINFILTERING_flt_chained_seeds_kernel(
  const mem_opt_t* __restrict__ d_opt, 	
     const bntseq_t* __restrict__ d_bns, 
     const uint8_t* __restrict__ d_pac, 
@@ -3219,7 +3219,7 @@ __global__ void CHAINFILTERING_flt_chained_seeds_kernel_new(
 	}
 }
 
-__global__ void CHAINFILTERING_flt_chained_seeds_kernel(
+__global__ void CHAINFILTERING_flt_chained_seeds_kernel_old(
 	const mem_opt_t *d_opt, const bntseq_t *d_bns, const uint8_t *d_pac, const bseq1_t *d_seqs,
 	mem_chain_v *d_chains, 	// input and output
 	int n,		// number of seqs
@@ -4354,10 +4354,21 @@ void mem_align_GPU(process_data_t *process_data)
 
 	/* fourth kernel: mem_flt_chained_seeds */
 	if (bwa_verbose>=4) fprintf(stderr, "[M::%-25s] **** [CHAIN FILTERING]: Launch kernel mem_flt_chained_seeds ...\n", __func__);
+	const int flt_blockSize = 128; // Standard block size (4 warps per block)
+	const int flt_warpsPerBlock = flt_blockSize / 32;
+
+	// We need enough blocks to cover all 'n' reads, where each block covers 'warpsPerBlock' reads.
+	const int flt_numBlocks = (n + flt_warpsPerBlock - 1) / flt_warpsPerBlock;
+
+	CHAINFILTERING_flt_chained_seeds_kernel<<<flt_numBlocks, flt_blockSize, 0, process_stream>>>(
+		d_opt, d_bns, d_pac, d_seqs, d_chains, n, d_buffer_pools
+	);
+	/**
 	CHAINFILTERING_flt_chained_seeds_kernel <<< dimGrid_readlevel, dimBlock_readlevel, 0, process_stream >>> (
 			d_opt, d_bns, d_pac,
 			d_seqs, d_chains, 	// input and output
 			n_seqs, d_buffer_pools);
+			*/
 	gpuErrchk2( cudaPeekAtLastError() );
 	gpuErrchk2( cudaStreamSynchronize(process_stream) );
 
