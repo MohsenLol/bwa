3a6dce65df2695a7f824d38bfb693cf990d64277
PSalehi <pouyasalehieleceng@gmail.com>
2026-02-11 11:42:25 +0300

    derefrencing correctly

--- cuda/bwamem_GPU.cu
+++ cuda/bwamem_GPU.cu
@@ -2526,7 +2526,7 @@ __global__ void SEEDCHAINING_chain_kernel(
 		// 128 aligned copy
 			int4* src_ptr = (int4*)seed_a_global;
 			int4  raw4int = src_ptr[i << 1];
-			(int4*)&S_seeds[i] = raw4int;
+			*((int4*)&S_seeds[i]) = raw4int;
 			S_suceeding_seed[i] = INT_MAX; // initial: no chain yet
 	}
 //	return; // test_point 1
